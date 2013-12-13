8ff1d54e6ffa4d73e149b30ee6a99a5e1173b50a
JamesKid <406358964@qq.com>
2013-12-13 15:15:23 +0800

    second

--- Vbird/sh01_print_hello_world.sh
+++ Vbird/sh01_print_hello_world.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Description:  this program is to print hello world in the screen
-# Editor:   zhangshijie
+# Editor:   zhangshijie Kidicc
 # Version:  1.0
 # Date:     2013.12.5
+
 
 echo -e "hello world! \a \n "
 exit 0 
