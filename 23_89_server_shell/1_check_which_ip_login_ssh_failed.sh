b02054c9c409a20b5adc78ade89056c14adfb078
JamesKid <406358964@qq.com>
2014-06-27 16:03:14 +0800

    add check which ip login ssh failed

--- 23_89_server_shell/1_check_which_ip_login_ssh_failed.sh
+++ 23_89_server_shell/1_check_which_ip_login_ssh_failed.sh
@@ -1,2 +1,2 @@
-cat secure | grep Failed | awk '{print $11}' | grep '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | sort -n | uniq> /tmp/fail.txt
+cat secure | grep Failed | awk '{print $11}' | grep '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | sort -n | uniq > /tmp/fail.txt
 
